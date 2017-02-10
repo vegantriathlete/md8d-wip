4e5d71984135715aa5a76d75d51cc558ce37f91c
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-02-10 11:03:37 -0700

    Complete the individual WEA item resource logic
    I did need to use the language manager!
    This commit completes the resource and the two pseudo clients.

---
+++ pseudo_client/get/get_item_from_custom_code_en_espanol.php
@@ -0,0 +1,57 @@
+<?php
+
+// Execute a cURL call
+
+$rest_uri = 'http://testmd8ddev/es/wea/actions/22?_format=json';
+$curlExecutor = new curlExecutor($rest_uri);
+$results = $curlExecutor->getRecords();
+$decoded_results = json_decode($results);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>"GET"ing data from WEA module</title>
+  </head>
+  <body>
+    <h1>Eso si que es!</h1>
+<?php
+  echo "<ul>";
+  echo "<li>Titulo: " . $decoded_results->title . "</li>";
+  echo "<li>Coordenadas: " . $decoded_results->coordinates . "</li>";
+  echo "<li>Descripcion: " . $decoded_results->description . "</li>";
+  echo "</ul>";
+  echo "<p><pre>" . print_r($decoded_results, 1) . "</pre></p>";
+?>
+  </body>
+</html>
+<?php
+// Yippee! We successfully completed the script :)
+exit(0);
+
+/**
+ * cURL Executor
+ */
+class curlExecutor {
+  public $restURI;
+
+  public function __construct(string $rest_uri) {
+    $this->restURI = $rest_uri;
+  }
+
+  public function getRecords() {
+    // Setup the cURL request.
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->restURI);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    // Report any errors
+    if ($error = curl_error($ch)) {
+      $error_message = "cURL error at $this->endPoint: $error";
+      throw new Exception($error_message);
+    }
+    curl_close($ch);
+    return $result;
+  }
+
+}
--- wea/src/Plugin/rest/resource/WEAResource.php
+++ wea/src/Plugin/rest/resource/WEAResource.php
@@ -2,10 +2,15 @@
 
 namespace Drupal\wea\Plugin\rest\resource;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\node\Entity\Node;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Provides a resource for a water eco action item.
@@ -14,41 +19,91 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
  *   id = "wea_resource",
  *   label = @Translation("Water eco action item"),
  *   uri_paths = {
- *     "canonical" = "/wea/actions/{node}",
+ *     "canonical" = "/wea/actions/{id}",
  *   }
  * )
  */
 class WEAResource extends ResourceBase {
 
   /**
+   * The currently selected language.
+   */
+  protected $currentLanguage;
+
+  /**
+   * Constructs a Drupal\wea\Plugin\rest\resource\WEAResourceList object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, $serializer_formats, LoggerInterface $logger, LanguageManagerInterface $language_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->currentLanguage = $language_manager->getCurrentLanguage();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('language_manager')
+    );
+  }
+
+  /**
    * Responds to entity GET requests.
    *
    * Returns a water eco action item for the specified ID.
    *
-   * @param \Drupal\node\Entity\Node $node
-   *   The node object.
+   * @param string $id
+   *   The ID of the object.
    *
    * @return \Drupal\rest\ResourceResponse
    *   The response containing the entity with its accessible fields.
    *
    * @throws \Symfony\Component\HttpKernel\Exception\HttpException
    */
-  public function get(Node $node) {
-    $node_access = $node->access('view', NULL, TRUE);
-    if (!$node_access->isAllowed()) {
-      throw new AccessDeniedHttpException();
-    }
-$record = [
-  'id' => $id
-  'type' => $node->getType();
-  'title' => $node->getTitle();
-];
-      if (!empty($record)) {
-        return new ResourceResponse($record);
+  public function get($id) {
+    if ($node = Node::load($id)) {
+      $translated_node = $node->getTranslation($this->currentLanguage->getId());
+      $node_access = $translated_node->access('view', NULL, TRUE);
+      if (!$node_access->isAllowed()) {
+        throw new AccessDeniedHttpException();
       }
-
+      if ($node->getType() == 'water_eco_action') {
+        $record = [
+          'title' => $translated_node->getTitle(),
+          'description' => $translated_node->field_wea_description->value,
+          'coordinates' => $translated_node->field_wea_coordinates->value
+        ];
+      }
     }
 
+    if (!empty($record)) {
+      $response = new ResourceResponse($record, 200);
+      $response->addCacheableDependency($record);
+      return $response;
+    }
+
+    throw new NotFoundHttpException(t('Water eco action item with ID @id was not found', array('@id' => $id)));
   }
 
 }
