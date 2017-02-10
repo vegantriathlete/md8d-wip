6645c7fa9bc91d0fde6418e85cd15f03021ad4d5
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-02-09 19:19:58 -0700

    Add Language Manager logic
    I do believe that the resource to list water eco action items will need
    the language manager to load the currently selected language so that I
    can pass a condition to the EntityQuery to match (and filter) the nodes
    based on it.

--- wea/src/Plugin/rest/resource/WEAResourceList.php
+++ wea/src/Plugin/rest/resource/WEAResourceList.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\wea\Plugin\rest\resource;
 
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a resource to list water eco action items.
@@ -17,6 +19,50 @@ use Drupal\rest\ResourceResponse;
  * )
  */
 class WEAResourceList extends ResourceBase {
+
+  /**
+   * The currently selected language.
+   */
+  protected $currentLanguage;
+
+  /**
+   * Constructs a Drupal\wea\Plugin\rest\resource\WEAResourceList object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, $serializer_formats, LoggerInterface $logger, LanguageManagerInterface $language_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->languageManager = $language_manager;
+    $this->language = $this->language_manager->getCurrentLanguage();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('language_manager')
+    );
+  }
 
   /**
    * Responds to GET requests.
