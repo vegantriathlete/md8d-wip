a85eb7a49b0408a5024da0759d238067cfaf8907
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-02-09 17:24:38 -0700

    Remove the Language Manager logic
    The only thing this commit does is to remove all the Language Manager
    logic that I don't think I'm going to need. All the rest of the code is
    the same. I haven't actually tested any of it, though.

--- wea/src/Plugin/rest/resource/WEAResource.php
+++ wea/src/Plugin/rest/resource/WEAResource.php
@@ -2,11 +2,9 @@
 
 namespace Drupal\wea\Plugin\rest\resource;
 
-use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\node\Entity\Node;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
@@ -21,57 +19,6 @@ use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
  * )
  */
 class WEAResource extends ResourceBase {
-
-  /**
-   * The language manager.
-   *
-   * @var \Drupal\Core\Language\LanguageManagerInterface
-   */
-  protected $languageManager;
-
-  /**
-   * The currently selected language.
-   */
-  protected $currentLanguage;
-
-  /**
-   * Constructs a Drupal\wea\Plugin\rest\resource\WEAResource object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager
-   * @param array $serializer_formats
-   *   The available serialization formats.
-   * @param \Psr\Log\LoggerInterface $logger
-   *   A logger instance.
-   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
-   *   The config factory.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, $serializer_formats, LoggerInterface $logger, LanguageManagerInterface $language_manager) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
-    $this->languageManager = $language_manager;
-    $this->language = $this->language_manager->getCurrentLanguage();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('entity_type.manager'),
-      $container->getParameter('serializer.formats'),
-      $container->get('logger.factory')->get('rest'),
-      $container->get('language_manager')
-    );
-  }
 
   /**
    * Responds to entity GET requests.
