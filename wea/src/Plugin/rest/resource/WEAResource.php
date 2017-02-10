ad9c0e33fa345e983f8102d39c8dbe145f4a541a
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-02-09 17:22:05 -0700

    Save Language Manager logic
    I don't think I'm going to need the logic because I'm typehinting with
    Node for the get method. But, I want to have all this code somewhere in
    case I do end up needing to retrieve it. I haven't done any testing with
    it so I don't know whether it works or not.

--- wea/src/Plugin/rest/resource/WEAResource.php
+++ wea/src/Plugin/rest/resource/WEAResource.php
@@ -2,11 +2,12 @@
 
 namespace Drupal\wea\Plugin\rest\resource;
 
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\node\Entity\Node;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
-use Drupal\rest\ModifiedResourceResponse;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 /**
  * Provides a resource for a water eco action item.
@@ -15,57 +16,92 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  *   id = "wea_resource",
  *   label = @Translation("Water eco action item"),
  *   uri_paths = {
- *     "canonical" = "/wea/actions/{id}",
- *     "https://www.drupal.org/link-relations/create" = "/wea/actions"
+ *     "canonical" = "/wea/actions/{node}",
  *   }
  * )
  */
 class WEAResource extends ResourceBase {
 
   /**
-   * Responds to GET requests.
+   * The language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  protected $languageManager;
+
+  /**
+   * The currently selected language.
+   */
+  protected $currentLanguage;
+
+  /**
+   * Constructs a Drupal\wea\Plugin\rest\resource\WEAResource object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The config factory.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, $serializer_formats, LoggerInterface $logger, LanguageManagerInterface $language_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->languageManager = $language_manager;
+    $this->language = $this->language_manager->getCurrentLanguage();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('language_manager')
+    );
+  }
+
+  /**
+   * Responds to entity GET requests.
    *
    * Returns a water eco action item for the specified ID.
    *
-   * @param int $id
-   *   The ID of the water eco action item.
+   * @param \Drupal\node\Entity\Node $node
+   *   The node object.
    *
    * @return \Drupal\rest\ResourceResponse
-   *   The response containing the log entry.
+   *   The response containing the entity with its accessible fields.
    *
-   * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
-   *   Thrown when the log entry was not found.
-   * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
-   *   Thrown when no log entry was provided.
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
    */
-  public function get($id = NULL) {
-    if ($id) {
-      //$record = db_query("SELECT * FROM {watchdog} WHERE wid = :wid", array(':wid' => $id))
-      //  ->fetchAssoc();
+  public function get(Node $node) {
+    $node_access = $node->access('view', NULL, TRUE);
+    if (!$node_access->isAllowed()) {
+      throw new AccessDeniedHttpException();
+    }
 $record = [
   'id' => $id
+  'type' => $node->getType();
+  'title' => $node->getTitle();
 ];
       if (!empty($record)) {
         return new ResourceResponse($record);
       }
 
-      //throw new NotFoundHttpException(t('Log entry with ID @id was not found', array('@id' => $id)));
     }
 
-    // As near as I can tell, it's not possible to get to this method without
-    // providing and ID. When I access the path without an ID I get a "No route
-    // found" message. I don't know why DBLogResource throws this error.
-    throw new BadRequestHttpException(t('No Water Eco Action ID was provided'));
-  }
-
-  public function post() {
-$entity = [
-  'id' => '1',
-  'title' => 'Yay I just created a new WEA!',
-  'description' => 'This is a hard-coded wea to test the post method.'
-];
-    $response = new ModifiedResourceResponse($entity, 201, ['Location' => 'http://testmd8ddev/action/yay-created-new-wea']);
-    return $response;
   }
 
 }
