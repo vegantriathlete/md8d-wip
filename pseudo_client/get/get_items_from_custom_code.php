0635896c40d4d1d764cf61b375f98608678d3239
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-02-10 15:30:07 -0700

    Create the pseudo clients for the item listing
    This is the English and Spanish version of the clients that poll the
    WEA Resource Listing REST URI.

---
+++ pseudo_client/get/get_items_from_custom_code.php
@@ -0,0 +1,57 @@
+<?php
+
+// Execute a cURL call
+
+$rest_uri = 'http://testmd8ddev/wea/actions?_format=json';
+$curlExecutor = new curlExecutor($rest_uri);
+$results = $curlExecutor->getRecords();
+$decoded_results = json_decode($results);
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>"GET"ing WEA list from custom code</title>
+  </head>
+  <body>
+    <h1>This is the data received.</h1>
+<?php
+foreach ($decoded_results as $decoded_result) {
+  echo "<ul>";
+  echo "<li>ID: " . $decoded_result->id . "</li>";
+  echo "<li>Title: " . $decoded_result->title . "</li>";
+  echo "</ul>";
+}
+?>
+  </body>
+</html>
+<?php
+// Yippee! We successfully completed the script :)
+exit(0);
+
+/**
+ * cURL Executor
+ */
+class curlExecutor {
+  public $restURI;
+
+  public function __construct(string $rest_uri) {
+    $this->restURI = $rest_uri;
+  }
+
+  public function getRecords() {
+    // Setup the cURL request.
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->restURI);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    // Report any errors
+    if ($error = curl_error($ch)) {
+      $error_message = "cURL error at $this->endPoint: $error";
+      throw new Exception($error_message);
+    }
+    curl_close($ch);
+    return $result;
+  }
+
+}
