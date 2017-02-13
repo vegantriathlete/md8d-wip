7f78e799d8bf9e74fc6dce479b30bbee9fd24aff
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-02-13 01:54:29 -0700

    Write client to POST via Drupal core
    Note: I cloned get_from_drupal_core into get_from_drupal_core_with_hal
          , changing it to use _format=json_hal, so that we can inspect the
          '_links' structure. I didn't want to directly modify
          get_from_drupal_core because that would mess things up for the
          section on GET.

--- pseudo_client/post/post_via_drupal_core.php
+++ pseudo_client/post/post_via_drupal_core.php
@@ -2,48 +2,101 @@
 
 // Execute a cURL call
 
-// Just testing stuff with https://jsonplaceholder.typicode.com/
-// @see: https://github.com/typicode/jsonplaceholder#creating-a-resource
-$api_endpoint = 'http://jsonplaceholder.typicode.com/posts';
+// @see: https://www.drupal.org/docs/8/core/modules/rest/3-post-for-creating-content-entities
+//       As of Drupal 8.3.0 you may use /node instead of /entity/node.
+$rest_uri = 'http://testmd8ddev/entity/node?_format=hal_json';
+$timestamp = date('F j, Y g:i a');
+// Drupal requires us to supply the '_links' key. The easiest way to figure out
+// how to build the links key is to first to a "GET" on the node type you wish
+// to build and examine the response. Doing this also helps you figure out how
+// to build the rest of the data.
 $post_fields = array(
-  'data' => array(
-    'title' => 'foo',
-    'body' => 'bar',
-    'userId' => 1,
+  '_links' => array(
+    'type' => array(
+      'href' => 'http://testmd8ddev/rest/type/node/water_eco_action',
+    ),
   ),
+  'title' => array(0 => array('value' => 'My POSTed WEA - ' . $timestamp)),
+  'type' => array(0 => array('target_id' => 'water_eco_action')),
+  'langcode' => array(0 => array('value' => 'en')),
+  'field_wea_description' => array(0 => array('value' => 'I successfully created this with a POST operation at ' . $timestamp . '!')),
 );
-$curlExecutor = new curlExecutor($api_endpoint, $post_fields);
+$tokenRetriever = new tokenRetriever();
+$token = $tokenRetriever->getToken();
+$curlExecutor = new curlExecutor($rest_uri, $token, $post_fields);
 $result = $curlExecutor->postFields();
-var_dump($result);
 $decoded_result = json_decode($result);
-var_dump($decoded_result);
-
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>"POST"ing data via Drupal core</title>
+  </head>
+  <body>
+    <h1>Here is some lovely information for you!</h1>
+    <h2>This is the data we sent.</h2>
+<?php
+echo "<pre>" . print_r($post_fields, 1) . "</pre>";
+?>
+    <h2>This is the data we received.</h2>
+<?php
+echo "<pre>" . print_r($decoded_result, 1) . "</pre>";
+?>
+  </body>
+</html>
+<?php
 // Yippee! We successfully completed the script :)
 exit(0);
+
+/**
+ * cURL Token Retriever
+ */
+class tokenRetriever {
+  public function getToken() {
+    // Setup the cURL request.
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'http://testmd8ddev/session/token');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    // Report any errors
+    if ($error = curl_error($ch)) {
+      $error_message = "cURL error at $this->endPoint: $error";
+      throw new Exception($error_message);
+    }
+    curl_close($ch);
+    return $result;
+  }
+}
 
 /**
  * cURL Executor
  */
 class curlExecutor {
-  public $endPoint;
+  public $restURI;
   public $postFields;
-
-  public function __construct(string $api_endpoint, array $post_fields) {
-    $this->endPoint = $api_endpoint;
+  public function __construct(string $rest_uri, string $token, array $post_fields) {
+    $this->restURI = $rest_uri;
+    $this->token = $token;
     $this->postFields = $post_fields;
   }
 
   public function postFields() {
     // Setup the cURL request.
+    $data = json_encode($this->postFields);
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $this->endPoint);
-    curl_setopt($ch, CURLOPT_VERBOSE, 0);
+    curl_setopt($ch, CURLOPT_URL, $this->restURI);
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($this->postFields));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_NOPROGRESS, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    curl_setopt($ch, CURLOPT_USERPWD, 'rest_user:rest_user');
+    // We need to set the header at the end because PHP cURL sets it to
+    // 'Content-type: application/x-www-form-urlencoded'
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Content-type: application/hal+json',
+      'X-CSRF-Token: ' . $this->token
+    ));
     $result = curl_exec($ch);
     // Report any errors
     if ($error = curl_error($ch)) {
