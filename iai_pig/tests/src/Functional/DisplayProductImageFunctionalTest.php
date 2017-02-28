57319bc240e6e9a23f55678c347a3bd85d705fae
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-02-28 15:27:00 -0700

    Complete the Functional test

--- iai_pig/tests/src/Functional/DisplayProductImageFunctionalTest.php
+++ iai_pig/tests/src/Functional/DisplayProductImageFunctionalTest.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\Tests\iai_pig\Functional;
 
-use Drupal\file\Entity\File;
 use Drupal\Tests\BrowserTestBase;
 
 /**
@@ -22,7 +21,6 @@ class DisplayProductImageFunctionalTest extends BrowserTestBase {
   /**
    * Tests a product that does not have an image attached.
    */
-/*
   public function testProductWithoutImage() {
 
     // Create a product piece of content
@@ -35,30 +33,22 @@ class DisplayProductImageFunctionalTest extends BrowserTestBase {
     $this->drupalGet('iai_pig/display_product_image/' . $node->id() . '/0');
     $this->assertContains($message, $this->getTextContent());
   }
-*/
 
   /**
    * Tests a product that has an image attached.
    */
-/**/
   public function testProductWithImage() {
 
     // Create a product piece of content
     $node = $this->drupalCreateNode(array(
       'title' => t('Product with an image'),
-      'type' => 'test_product',
+      'type' => 'product',
     ));
-    $node->field_test_product_image->generateSampleItems();
-//$node->field_test_product_image->generateSampleItems(3);
+    $node->field_image->generateSampleItems();
     $node->save();
-//$image_data = $node->field_test_product_image->getValue();
-    $message = 'You are viewing ' . $node->getTitle() . '. Unfortunately, there is no image defined for delta: 0.';
 
     $this->drupalGet('iai_pig/display_product_image/' . $node->id() . '/0');
-    $pageText = $this->getTextContent();
-    $this->assertContains('lmnopqrstuv', $pageText);
-//$this->assertSame('debug', $image_data);
+    $this->assertSession()->responseContains('product-images/generateImage');
   }
-/**/
 
 }
