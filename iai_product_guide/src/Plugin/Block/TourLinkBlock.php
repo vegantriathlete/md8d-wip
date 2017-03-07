7e8ad09e05404f0958a9126f5c97a635f1c3d014
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-03-07 13:13:04 -0700

    Create product guide tour
    Instead of using the Forums module and creating a tour for it, I have
    decided to skip the Forums entirely. I've created a tour for the product
    guides to teach about the Tour API.

---
+++ iai_product_guide/src/Plugin/Block/TourLinkBlock.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\iai_product_guide\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a block with a link to start the tour.
+ *
+ * @Block(
+ *   id = "iai_tour_link",
+ *   admin_label = @Translation("Link for the IAI Product Guide tour"),
+ *   category = @Translation("Links"),
+ *   context = {
+ *     "node" = @ContextDefinition(
+ *       "entity:node",
+ *       label = @Translation("Current Node")
+ *     )
+ *   }
+ * )
+ */
+class TourLinkBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Constructs an TourLinkBlock object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, AccountInterface $current_user) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $current_user;
+  }
+
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_user')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $node = $this->getContextValue('node');
+    if ($this->currentUser->hasPermission('access tour') && $node->getType() == 'book') {
+      $url = Url::fromRoute('<current>', array(), array('query' => array('tour' => 1)));
+      $build['tour_link']= [
+        '#type' => 'markup',
+        '#markup' => Link::fromTextAndUrl(t('Take the tour!'), $url)->toString(),
+      ];
+      $build['#attached']['library'][] = 'tour/tour';
+      return $build;
+    }
+  }
+
+}
