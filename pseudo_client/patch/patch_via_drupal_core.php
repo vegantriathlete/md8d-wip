b4e41cb071cf4f7d7a64450479837bf1393e313f
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-04-24 13:25:31 -0600

    Accept domain, item and format as query args

--- pseudo_client/patch/patch_via_drupal_core.php
+++ pseudo_client/patch/patch_via_drupal_core.php
@@ -1,8 +1,29 @@
 <?php
 
-// Execute a cURL call
+/******************************************************************************
+ **                                                                          **
+ ** Remember to include the domain as a query argument!                      **
+ ** Remember to include the item number as a query argument!                 **
+ **                                                                          **
+ ******************************************************************************/
+$domain = $_GET['domain'];
+$item = $_GET['item'];
 
-$rest_uri = 'http://testmd8ddev/node/39?_format=hal_json';
+// See if a particular format was requested
+if (isset($_GET['format'])) {
+  $format = $_GET['format'];
+}  else {
+  $format = 'json';
+}
+
+/******************************************************************************
+ **                                                                          **
+ ** Note that we are appending the _format query argument. This argument     **
+ ** specifies the serialization format of Drupal's response, not the format  **
+ ** in which we will send the data. We MUST send the data in hal+json format.**
+ **                                                                          **
+ ******************************************************************************/
+$rest_uri = 'http://' . $domain . '/node/' . $item . '?_format=' . $format;
 $timestamp = date('F j, Y g:i a');
 // Drupal requires us to supply the '_links' key. The easiest way to figure out
 // how to build the links key is to first to a "GET" on the node type you wish
@@ -11,14 +32,19 @@ $timestamp = date('F j, Y g:i a');
 $post_fields = array(
   '_links' => array(
     'type' => array(
-      'href' => 'http://testmd8ddev/rest/type/node/water_eco_action',
+      'href' => 'http://' . $domain . '/rest/type/node/water_eco_action',
     ),
   ),
   'title' => array(0 => array('value' => 'My PATCHed WEA - ' . $timestamp)),
   'field_wea_description' => array(0 => array('value' => 'I successfully updated this with a PATCH operation at ' . $timestamp . '!')),
 );
-$tokenRetriever = new tokenRetriever();
+
+// Drupal (supposedly) requires a token in order to prevent Cross Site Request
+// Forgery.
+$tokenRetriever = new tokenRetriever($domain);
 $token = $tokenRetriever->getToken();
+
+// Execute a cURL call
 $curlExecutor = new curlExecutor($rest_uri, $token, $post_fields);
 $result = $curlExecutor->postFields();
 $decoded_result = json_decode($result);
@@ -49,10 +75,16 @@ exit(0);
  * cURL Token Retriever
  */
 class tokenRetriever {
+  public $domain;
+  public $restURI;
+  public function __construct(string $domain) {
+    $this->domain = $domain;
+    $this->restURI = 'http://' . $domain . '/session/token';
+  }
   public function getToken() {
     // Setup the cURL request.
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://testmd8ddev/session/token');
+    curl_setopt($ch, CURLOPT_URL, $this->restURI);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $result = curl_exec($ch);
     // Report any errors
@@ -83,10 +115,20 @@ class curlExecutor {
     $data = json_encode($this->postFields);
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $this->restURI);
+    // cURL doesn't have a CURLOPT_PATCH option.
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+/******************************************************************************
+ **                                                                          **
+ ** Look out! We are sending a clear text user:password combination. We'd    **
+ ** really want to:                                                          **
+ **   1) Pull our credentials from a protected file on the server            **
+ **   2) Use only sites that run secure http protocol (https://)             **
+ **                                                                          **
+ ******************************************************************************/
     curl_setopt($ch, CURLOPT_USERPWD, 'rest_user:rest_user');
     // We need to set the header at the end because PHP cURL sets it to
     // 'Content-type: application/x-www-form-urlencoded'
