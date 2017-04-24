957f9885e11f404f1d0dc3936f6ef73f7ef4219d
Marc Isaacson <marc@isaacsonwebdevelopment.com>
2017-04-24 16:32:25 -0600

    Accept domain, item and format as query arguments

--- pseudo_client/delete/delete_via_custom_code.php
+++ pseudo_client/delete/delete_via_custom_code.php
@@ -1,10 +1,43 @@
 <?php
 
-// Execute a cURL call
+/******************************************************************************
+ **                                                                          **
+ ** Remember to include the domain as a query argument!                      **
+ ** Remember to include the item number as a query argument!                 **
+ **                                                                          **
+ ******************************************************************************/
+$domain = $_GET['domain'];
+$item = $_GET['item'];
 
-$rest_uri = 'http://testmd8ddev/wea/actions/42?_format=json';
-$tokenRetriever = new tokenRetriever();
+// See if a particular format was requested
+if (isset($_GET['format'])) {
+  $format = $_GET['format'];
+}  else {
+  $format = 'json';
+}
+
+/******************************************************************************
+ **                                                                          **
+ ** Note that we are appending the _format query argument. This argument     **
+ ** specifies the serialization format of Drupal's response, not the format  **
+ ** in which we will send the data.                                          **
+ **                                                                          **
+ ** The URI we use is determined by the Annotation in our resource,          **
+ ** specifically the canonical entry.                                        **
+ **  uri_paths = {                                                           **
+ **    "canonical" = "/wea/actions/{id}",                                    **
+ **    "https://www.drupal.org/link-relations/create" = "/wea/actions"       **
+ **  }                                                                       **
+ **                                                                          **
+ ******************************************************************************/
+$rest_uri = 'http://' . $domain . '/wea/actions/' . $item . '?_format=' . $format;
+
+// Drupal (supposedly) requires a token in order to prevent Cross Site Request
+// Forgery.
+$tokenRetriever = new tokenRetriever($domain);
 $token = $tokenRetriever->getToken();
+
+// Execute a cURL call
 $curlExecutor = new curlExecutor($rest_uri, $token);
 $result = $curlExecutor->deleteContent();
 $decoded_result = json_decode($result);
@@ -31,10 +64,16 @@ exit(0);
  * cURL Token Retriever
  */
 class tokenRetriever {
+  public $domain;
+  public $restURI;
+  public function __construct(string $domain) {
+    $this->domain = $domain;
+    $this->restURI = 'http://' . $domain . '/session/token';
+  }
   public function getToken() {
     // Setup the cURL request.
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://testmd8ddev/session/token');
+    curl_setopt($ch, CURLOPT_URL, $this->restURI);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $result = curl_exec($ch);
     // Report any errors
@@ -62,9 +101,19 @@ class curlExecutor {
     // Setup the cURL request.
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $this->restURI);
+    // cURL doesn't have a CURLOPT_DELETE option.
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+/******************************************************************************
+ **                                                                          **
+ ** Look out! We are sending a clear text user:password combination. We'd    **
+ ** really want to:                                                          **
+ **   1) Pull our credentials from a protected file on the server            **
+ **   2) Use only sites that run secure http protocol (https://)             **
+ **                                                                          **
+ ******************************************************************************/
     curl_setopt($ch, CURLOPT_USERPWD, 'rest_user:rest_user');
     // We need to set the header at the end because PHP cURL sets it to
     // 'Content-type: application/x-www-form-urlencoded'
--- pseudo_client/delete/delete_via_drupal_core.php
+++ pseudo_client/delete/delete_via_drupal_core.php
@@ -1,10 +1,36 @@
 <?php
 
-// Execute a cURL call
+/******************************************************************************
+ **                                                                          **
+ ** Remember to include the domain as a query argument!                      **
+ ** Remember to include the item number as a query argument!                 **
+ **                                                                          **
+ ******************************************************************************/
+$domain = $_GET['domain'];
+$item = $_GET['item'];
 
-$rest_uri = 'http://testmd8ddev/node/40?_format=hal_json';
-$tokenRetriever = new tokenRetriever();
+// See if a particular format was requested
+if (isset($_GET['format'])) {
+  $format = $_GET['format'];
+}  else {
+  $format = 'json';
+}
+
+/******************************************************************************
+ **                                                                          **
+ ** Note that we are appending the _format query argument. This argument     **
+ ** specifies the serialization format of Drupal's response, not the format  **
+ ** in which we will send the data.                                          **
+ **                                                                          **
+ ******************************************************************************/
+$rest_uri = 'http://' . $domain . '/node/' . $item . '?_format=' . $format;
+
+// Drupal (supposedly) requires a token in order to prevent Cross Site Request
+// Forgery.
+$tokenRetriever = new tokenRetriever($domain);
 $token = $tokenRetriever->getToken();
+
+// Execute a cURL call
 $curlExecutor = new curlExecutor($rest_uri, $token);
 $result = $curlExecutor->deleteContent();
 $decoded_result = json_decode($result);
@@ -31,10 +57,16 @@ exit(0);
  * cURL Token Retriever
  */
 class tokenRetriever {
+  public $domain;
+  public $restURI;
+  public function __construct(string $domain) {
+    $this->domain = $domain;
+    $this->restURI = 'http://' . $domain . '/session/token';
+  }
   public function getToken() {
     // Setup the cURL request.
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'http://testmd8ddev/session/token');
+    curl_setopt($ch, CURLOPT_URL, $this->restURI);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $result = curl_exec($ch);
     // Report any errors
@@ -62,9 +94,19 @@ class curlExecutor {
     // Setup the cURL request.
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $this->restURI);
+    // cURL doesn't have a CURLOPT_DELETE option.
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+/******************************************************************************
+ **                                                                          **
+ ** Look out! We are sending a clear text user:password combination. We'd    **
+ ** really want to:                                                          **
+ **   1) Pull our credentials from a protected file on the server            **
+ **   2) Use only sites that run secure http protocol (https://)             **
+ **                                                                          **
+ ******************************************************************************/
     curl_setopt($ch, CURLOPT_USERPWD, 'rest_user:rest_user');
     // We need to set the header at the end because PHP cURL sets it to
     // 'Content-type: application/x-www-form-urlencoded'
